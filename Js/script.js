4039c536b9ff6b5943766e9a0f4c19008cef8248
Vini <128169661+ViniciusRib1@users.noreply.github.com>
2024-07-16 16:57:26 -0300

    Colocando o sobre e o gitignore

--- Js/script.js
+++ Js/script.js
@@ -2,9 +2,11 @@ function abrir() {
     let menu = document.getElementById("menulat");
     let menua = document.getElementById("menua")
     let logo = document.getElementById("logo")
+    let fecharitem = document.getElementById("closeinmenuitens");
     logo.style.margin = "25%"
     menu.style.display = "block"
     menua.style.display = "none"
+    fecharitem.style.display = "block"
  
 }
 function fechar() {
@@ -13,7 +15,9 @@ function fechar() {
     let logo = document.getElementById("logo")
     logo.style.margin = "10%"
     menu.style.display = "none"
+    let fecharitem = document.getElementById("closeinmenuitens");
     menua.style.display = "block"
+    fecharitem.style.display = "none"
 }
 
 function toggleSwitch() {
@@ -21,5 +25,26 @@ function toggleSwitch() {
     const dark = document.getElementById("darkmode");
 
     StyleSheet = light;
+}
+
+function calcIdade() {
+    let sobre = document.getElementById("sobre");
+    let dataAtual = new Date();
+    let nascimento = new Date(2005, 7, 9); 
+
+    let mesAtual = dataAtual.getMonth();
+    let mesNascimento = nascimento.getMonth();
+    let anoAtual = dataAtual.getFullYear();
+    let anoNascimento = nascimento.getFullYear();
+    let idade;
+    
+    if (mesAtual < mesNascimento) {
+        idade = anoAtual - anoNascimento - 1;
+        sobre.innerHTML = `Olá! Me chamo Vinicius Ribeiro, atualmente eu tenho ${idade} anos, eu sou um programador que adora estudar novas tecnologias`;
+    } else {
+        idade = anoAtual - anoNascimento;
+        sobre.innerHTML = `Olá! Me chamo Vinicius Ribeiro, atualmente eu tenho ${idade} anos, eu sou um programador que adora estudar novas tecnologias`;
+    }
     
 }
+calcIdade();
